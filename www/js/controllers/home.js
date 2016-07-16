95b37ccc3972278b9818fe4b98a693e810df108f
allencwwong <allencwwong@gmail.com>
2016-07-16 12:45:09 -0700

    test git

--- www/js/controllers/home.js
+++ www/js/controllers/home.js
@@ -2,7 +2,8 @@
 
 app.controller("HomeCtrl",function(Auth){
 
-// some kind of algorithom to display random food near by current location base on setting / fitler   
+// some kind of algorithom to display random food near by current location base on setting / fitler  
+// test git 
   
   var home = this;
   home.profiles = Auth.getProfiles();
